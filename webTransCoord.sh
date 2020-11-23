834e185433884751ca8f0eed647fbd9b40dec6bd
André Pinhal <33957753+apinhal@users.noreply.github.com>
2020-11-23 00:01:40 +0000

    url

--- webTransCoord.sh
+++ webTransCoord.sh
@@ -8,6 +8,9 @@ z=$3 # Opcional
 
 # "Portugal Continental" /  Acores / Madeira
 area="Portugal%20Continental"
+#area="Acores"
+#area="Madeira"
+
 
 crsin=$4 # 3763
 crsout=$5 # 4258
@@ -15,10 +18,16 @@ crsout=$5 # 4258
 #  if z used
 altin="Elipsoidal" 
 altout="Ortometrica"
+#altout="Elipsoidal"
 
 # Datum Transform
-metodo="grelhas"
-metodo="bursaWolf"
+#metodo="grelhas" (default)
+#metodo="bursaWolf"
 
-# run
-curl -s http://cgpr.dgterritorio.pt/webtranscoord/transformar\?x\=$x\&y\=$y\&z\=$z\&area\=$area\&crsin\=$4\&crsout\=$5\&altin\=$altin\&altout\=$altout\&metodo\=$metodo | awk -F';' '{print $1" "$2" "$3}'
+fullURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=$x&y=$y&z=$z&area=$area&crsin=$4&crsout=$5&altin=$altin&altout=$altout&metodo=$metodo"
+echo $fullURL
+curl -s $fullURL | awk -F';' '{print $1" "$2" "$3}'
+
+#xyURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=$x&y=$y&area=$area&crsin=$4&crsout=$5&metodo=$metodo"
+#echo $xyURL
+#curl -s $xyURL | awk -F';' '{print $1" "$2}'
