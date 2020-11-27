68530095c5baf85d099e9d006f821345e89b65cb
André Pinhal <33957753+apinhal@users.noreply.github.com>
2020-11-27 11:47:10 +0000

    while ok

--- WebTransCoord.sh
+++ WebTransCoord.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/bash -   
+#title          :WebTransCoord.sh
+#description    :Transformção de coordenadas com o serviço REST(WebTransCoord) da DGT
+#author         :apinhal
+#date           :20201127
+#=======
 
-# webTransCoord.sh 0 0 0 3763 4258
 
 # Text Style
 tRED=$'\033[1;31m'      # Bold Light Red
@@ -8,24 +12,18 @@ tGREEN=$'\033[1;32m'    # Bold Light Green
 tBold=$'\033[1m'        # Bold
 tNS=$'\033[0m'          # No Style
 
+
 #
 ## usage
 if [ "$1" == "" ] || [ "$1" == "-h" ]; then
     
-    echo "${tBold}usage:${tNS} ${0##*/} crsin crsout"
+    echo "${tBold}usage:${tNS} ${0##*/} crsin crsout Elipsoidal Ortometrica"
     exit 1
 fi
 
-#x=$1
-#y=$2
-#z=$3 # optional
-# if z used
-altin="Elipsoidal"
-altout="Ortometrica"
-#altout=""
-#altout="Elipsoidal"
 
-
+#
+## CRS available
 # "Portugal Continental" /  Acores / Madeira
 area="Portugal%20Continental"
 #EPSG
@@ -47,7 +45,9 @@ area="Portugal%20Continental"
 #area="Madeira"
 # 5016 - ITRF93/PTRA08_UTM28N
 # 3061 - DatumBaseSE/UTM28N
-
+#
+#
+## CRS i/o
 crsin=$1 #$4 # 3763
 crsout=$2 #$5 # 4258
 
@@ -56,30 +56,53 @@ crsout=$2 #$5 # 4258
 metodo="grelhas" #(default)
 #metodo="bursaWolf"
 
-#fullURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=$x&y=$y&z=$z&area=$area&crsin=$4&crsout=$5&altin=$altin&altout=$altout&metodo=$metodo"
-#echo $fullURL
-#curl -s $fullURL | awk -F';' '{print $1" "$2" "$3}'
 
-#xyURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=$x&y=$y&area=$area&crsin=$4&crsout=$5&metodo=$metodo"
-#echo $xyURL
-#curl -s $xyURL | awk -F';' '{print $1" "$2}'
-
-
-
-
-reg='^[+-]?[0-9]+([.][0-9]+)?$' # decimal n regex
-while :
-do
-
-	read -a coord
+# if z used
+#altin="Elipsoidal"
+#altout="Ortometrica"
+altin=$3
+altout=$4
+#altout=""
+#altout="Elipsoidal"
 
 
-	if [[ ${coord[0]} =~ $reg ]] && [[ ${coord[1]} =~ $reg ]] && [[ ${coord[2]} =~ $reg ]]; then
+getWTC() { # func REST request
 	
-		fullURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=${coord[0]}&y=${coord[1]}&z=${coord[2]}&area=$area&crsin=$crsin&crsout=$crsout&altin=$altin&altout=$altout&metodo=$metodo"
-		#echo $fullURL
-		curl -s $fullURL | awk -F';' '{print $1" "$2" "$3}'
-	else
-		echo 'input error'
-	fi
+	fullURL="http://cgpr.dgterritorio.pt/webtranscoord/transformar?x=$1&y=$2&z=$3&area=$area&crsin=$crsin&crsout=$crsout&altin=$altin&altout=$altout&metodo=$metodo"
+	#echo $fullURL
+	curl -s $fullURL
+}
+
+errorPrint() {
+	
+	echo "${tRED}input error${tNS}"
+}
+
+
+reg="^[+-]?[0-9]+([.][0-9]+)?$"   # regex: decimal number 
+while IFS=" " read -r -a xyz; do  # input 2d or 3d	
+
+	case ${#xyz[@]} in # check number of elements of array: xyz 
+	2)
+	 if [[ ${xyz[0]} =~ $reg ]] && [[ ${xyz[1]} =~ $reg ]]; then # validate 2d input
+
+		getWTC ${xyz[0]} ${xyz[1]} 0 | awk -F';' '{print $1" "$2}'
+	 else
+	 	errorPrint
+	 fi
+	;;
+
+	3)
+	 if [[ ${xyz[0]} =~ $reg ]] && [[ ${xyz[1]} =~ $reg ]] && [[ ${xyz[2]} =~ $reg ]]; then # validate 3d input
+	
+		getWTC ${xyz[0]} ${xyz[1]} ${xyz[2]} | awk -F';' '{print $1" "$2" "$3}'
+	 else
+		errorPrint
+	 fi
+	 ;;
+
+	*)
+	 errorPrint
+	esac
+
 done
